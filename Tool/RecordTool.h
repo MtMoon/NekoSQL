b604f99ad727dc30bf7a6ca678f833b3c68fa0da
aaddc8d1cc414beeb949 <yxyz2012yxy@163.com>
2015-11-13 22:54:05 +0800

    Attention! struct tableinfo changedgit add -A!

--- Tool/RecordTool.h
+++ Tool/RecordTool.h
@@ -29,6 +29,7 @@ typedef pair<int,int> LP; //location air 用以定位一条数据记录，first�
  *定义一个表的固定基本信息
  *这部分信息存在表对应文件的第一页
  *注意，会变化的信息，如表中页数，不在此结构体中保存
+ *Attention! 2015.11.13 增加Vlen和keys
  */
 struct tableinfo {
 	int FN; // fixed-length column num
@@ -36,6 +37,8 @@ struct tableinfo {
 	string* Fname; // 定长列名数组
 	string* Vname; //变长列名数组
 	int*  Flen; //定长数据长度
+	int* Vlen; //变长数据的最大长度
+	int* keys; //每个字段的键类型 0为不是key，1为一般key，2为primary key 参照下面的fieldInfo
 	Byte* nullMap;
 };
 
